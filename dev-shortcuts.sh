cc8d042fedc36bb674e13010502dd4e7c2fe34df
Tony Spataro <tony@rightscale.com>
2014-05-21 11:19:36 -0700

    Add 'bc' shortcut for Bundler console; add $GOROOT to 'g' shortcut

--- dev-shortcuts.sh
+++ dev-shortcuts.sh
@@ -4,6 +4,7 @@ alias ups="ps aux | grep tony | grep -Ev '/(Library|System|Applications)'"
 
 alias r="bundle exec rake"
 alias bx="bundle exec"
+alias bc="bundle console"
 
 alias review="git diff --color=always --word-diff=color --inter-hunk-context=50"
 
@@ -11,6 +12,7 @@ function g {
   right_x="$HOME/Code/RightScale/right_$1"
   normal_x="$HOME/Code/RightScale/$1"
   other_x="$HOME/Code/Other/$1"
+  go_x="$HOME/Code/go/src/github.com/rightscale/$1"
 
   if [ -d $right_x ]; then
     cd $right_x
@@ -18,7 +20,12 @@ function g {
     cd $normal_x
   elif [ -d $other_x ]; then
     cd $other_x
+  elif [ -d $go_x ]; then
+    cd $go_x
   else
-    echo "Error: can't find $right_x or $normal_x or $other_x"
+    echo "Error: can't find $right_x, $normal_x, $other_x or $go_x"
   fi
 }
+
+# see https://github.com/rightscale/right_site/tree/master/spec/spec_helper.rb
+export FAST=1
