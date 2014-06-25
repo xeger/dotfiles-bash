1f7c1041d6636fc2ba05227fb75baa1f5c06b4cb
Tony Spataro <tony@rightscale.com>
2014-06-25 00:52:09 -0700

    Be more savvy about boot2docker detection

--- boot2docker.sh
+++ boot2docker.sh
@@ -1,5 +1,4 @@
-boot2docker && {
-  echo Initializing boot2docker...
+which -s boot2docker && {
   boot2docker init
   boot2docker start
   export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
