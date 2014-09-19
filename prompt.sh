df996a95130e160eda71c78a220daf9ccafbe08c
Tony Spataro <tony@rightscale.com>
2014-09-19 14:31:38 -0700

    Add PWD awesomeness to Darwin prompt

--- prompt.sh
+++ prompt.sh
@@ -1,7 +1,8 @@
 if [ `uname -s` == "Darwin" ]
 then
-  # Awesome Mac OS Terminal prompt that uses xterm-256 color and Unicode color emoji
-  PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[38;5;27m\]\w$(__git_ps1 " (%s)")\[\033[00m\] 👌  ' 
+  # Awesome Mac OS Terminal prompt that uses xterm-256 color, Unicode color emoji
+  # and Terminal tab naming after PWD.
+  PS1='\e]1;$(basename `pwd`)\a\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[38;5;27m\]\w$(__git_ps1 " (%s)")\[\033[00m\] 👌  ' 
 else
   #Purdy git-aware prompt that works on any *nix system
   PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[00;34m\]\w$(__git_ps1 " (%s)")\[\033[00m\]\$ '
