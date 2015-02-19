4c2e4665e4f8db1634d3f7ac60bc4c8da17c1216
Tony Spataro <tony@rightscale.com>
2015-02-19 13:04:58 -0800

    Unify path/gopath stuff

--- golang.sh
+++ golang.sh
@@ -1,4 +1,6 @@
-if [ -d $HOME/Code/go/bin ]
+# I like to keep my GOPATH in a specific, consistent place
+if [ -d $HOME/Code/go ]
 then
+  export GOPATH=$HOME/Code/go
   export PATH=$PATH:$HOME/Code/go/bin
 fi
