804a217bd072cfcfbbda14ddc18ac1746b8d1d86
Tony Spataro <tony@rightscale.com>
2015-02-20 14:30:23 -0800

    Change prompt color

--- prompt.sh
+++ prompt.sh
@@ -6,8 +6,8 @@ export GIT_PS1_SHOWUPSTREAM=auto
 if [ `uname -s` == "Darwin" ]
 then
   # Awesome Mac OS Terminal prompt that uses xterm-256 color and Unicode color emoji.
-  # Bright yellow username reminds us we're not in a REAL work environment.
-  PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[38;5;27m\]\w$(__git_ps1 " (%s)")\[\033[00m\] 👌  ' 
+  # White username reminds us we're not in a REAL work environment.
+  PS1='\[\033[01;37m\]\u@\h\[\033[00m\]:\[\033[38;5;27m\]\w$(__git_ps1 " (%s)")\[\033[00m\] 👌  ' 
 else
   # Purdy git-aware prompt that works on any *nix system. Bright green username tells
   # us we are free to develop with impunity.
