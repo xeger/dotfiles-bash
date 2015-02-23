f0bfdfdda9b31647c33175283f93258e57fffba4
Tony Spataro <tony@rightscale.com>
2015-02-23 11:26:58 -0800

    Only ssh-add if agent is running

--- ssh.sh
+++ ssh.sh
@@ -1,11 +1,14 @@
 
-private_keys=`grep -El 'BEGIN [A-Z]+ PRIVATE KEY' ~/.ssh/*`
-if [[ $? == 0 && -n $private_keys ]]; then
-  ssh-add $private_keys 2> /dev/null
+if [ -n "$SSH_AUTH_SOCK" ]
+then
+  private_keys=`grep -El 'BEGIN [A-Z]+ PRIVATE KEY' ~/.ssh/*`
+  if [[ $? == 0 && -n $private_keys ]]; then
+    ssh-add $private_keys 2> /dev/null
 
-  if [[ $? != 0 ]]; then
-    echo "Could not auto-add private SSH keys"
-    echo "Please run this command by hand:"
-    echo "ssh-add $private_keys"
+    if [[ $? != 0 ]]; then
+      echo "Could not auto-add private SSH keys"
+      echo "Please run this command by hand:"
+      echo "ssh-add $private_keys"
+    fi
   fi
 fi
