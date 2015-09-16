cc7a29a95b83511fcf405d985d5343d4a9565c90
Tony Spataro <tony@rightscale.com>
2015-09-16 15:17:23 -0700

    Remove defunct REE support code. Add rbenv bin dir to path when it exists.

--- rbenv.sh
+++ rbenv.sh
@@ -1,48 +1,10 @@
 # Tell Ruby never to print deprecation warnings; they're useless to us.
 export RUBYOPT=-W0
 
-# Ruby Enterprise Edition needs some vicious and significant hacks in order to install and to build gems.
-# Let us activate the hacks when needed
-function use_ree() {
-  # Never bother with Tcl/Tk support when building Rubies
-  export RUBY_CONFIGURE_OPTS="--without-tk --without-tcl --with-out-ext=tcl --with-out-ext=tk"
-
-  # With OS X, make sure that C compilers find XCode headers and lib dirs.
-  #
-  # Prerequisites: XCode 5.x is installed and you have run the "xcode-select --install" command from
-  # Terminal.
-  #
-  # This is necessary with XCode 5.0 and above because headers and libs are no longer installed under
-  # /usr.
-  which xcrun > /dev/null
-  if [ $? == 0 ]; then
-    export CPPFLAGS="$CPPFLAGS -I$(xcrun --show-sdk-path)/usr/include"
-    export CFLAGS="$CFLAGS -I$(xcrun --show-sdk-path)/usr/include -L$(xcrun --show-sdk-path)/usr/lib"
-  fi
-
-  # With Homebrew
-  #
-  # Prerequisites: the following homebrew packages have been installed and, if necessary, force-linked
-  # using "brew link -f":
-  #     apple-gcc42
-  #     openssl
-  #     readline
-  #     zlib
-  #
-  # This is necessary on Mac OS X Lion (10.8) and above because some Unix libraries no longer ship with
-  # the OS.
-  which brew > /dev/null && apple_gcc42="$(brew --prefix)/bin/gcc-4.2"
-  if [ -f "$apple_gcc42" ]; then
-    export CC=$apple_gcc42
-    export RUBY_CONFIGURE_OPTS="$RUBY_CONFIGURE_OPTS --with-readline-dir=$(brew --prefix readline) --with-openssl-dir=$(brew --prefix openssl)"
-    export CPPFLAGS="-I$(brew --prefix)/include -L$(brew --prefix)/lib"
-    export CFLAGS=$CPPFLAGS
-  fi
-
-  use ree
-  version=`ruby --version`
-  echo "All ready to experience ancient Ruby $version ..."
-}
+if [ -d $HOME/.rbenv/bin ]
+then
+  export PATH=$HOME/.rbenv/bin:$PATH
+fi
 
 function use() {
   if [ -z "$1" ]; then
