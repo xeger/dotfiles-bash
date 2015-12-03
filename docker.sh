deabb11575da7f494a8fdf27293d998037187248
Tony Spataro <tony@rightscale.com>
2015-12-03 10:45:27 -0800

    Add back docker extension commands as shell functions

--- docker.sh
+++ docker.sh
@@ -1,8 +1,20 @@
-alias dit="docker it"
+function docker-it() {
+  echo `docker ps -a | head -n 2 | tail -n 1 | awk '{print $NF}'`
+}
+
+function docker-gc() {
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc spotify/docker-gc
+}
+
+function docker-shell() {
+  docker exec -t -i $1 /bin/bash --login
+}
+
+alias dc="docker-compose"
+alias dit="docker-it"
 alias drm="docker rm -f"
 alias drmi="docker rmi"
 alias dps="docker ps --format 'table {{.Names}}\t{{.Status}}\t{{.Ports}}'"
 alias di="docker images"
-alias dgc="docker gc ; docker gci"
-alias dsh="docker shell"
-
+alias dgc="docker-gc"
+alias dsh="docker-shell"
