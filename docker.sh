6072056b27a8eee8d383f852aa914ff507e28f69
Tony Spataro <tony@rightscale.com>
2016-08-26 11:16:59 -0700

    Make dsh shortcut a bit smarter. Add cmdb alias.

--- docker.sh
+++ docker.sh
@@ -1,3 +1,21 @@
+
+function docker-find-container() {
+  running=`docker ps --format '{{.Names}}'`
+  declare -a matches
+
+  for c in $running; do
+    if [[ $c == *"$1"* ]]; then
+      matches=("${matches[@]}" "$c")
+    fi
+  done
+  echo ${matches[@]}
+  if [ ${#matches[@]} -gt 1 ]; then
+    return 1 
+  else
+    return 0
+  fi
+}
+
 function docker-it() {
   echo `docker ps -a | head -n 2 | tail -n 1 | awk '{print $NF}'`
 }
@@ -7,7 +25,13 @@ function docker-gc() {
 }
 
 function docker-shell() {
-  docker exec -t -i $1 /bin/bash --login
+  target=`docker-find-container $1`
+  if [ $? == 0 ]; then
+    docker exec -t -i $target /bin/bash --login
+  else
+    echo "Too many matching container names; please be more specific to choose one of:"
+    echo "  $target"
+  fi
 }
 
 alias dc="docker-compose"
@@ -20,3 +44,5 @@ alias dps="docker ps --format 'table {{.Names}}\t{{.Status}}\t{{.Ports}}'"
 alias di="docker images"
 alias dgc="docker-gc"
 alias dsh="docker-shell"
+alias cmdb="docker run -t -i --net=host rightscale/cmdb:latest"
+
